30d585bef553298c5eef28e901f4e66a2be8243c
Katy Harrod <m.catherine.harrod@gmail.com>
2022-03-04 14:09:30 -0700

    Refactor spec testing for API

--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'merchant discount index page' do
-  it 'has path' do
+  it 'has a route and header' do
     merchant = create(:merchant)
     visit "/merchants/#{merchant.id}/discounts"
-    expect(page).to have_content("vingus")
+    expect(page).to have_content("Discount index")
+  end
+
+  it 'displays upcoming holidays' do
+    merchant = create(:merchant)
+    visit "/merchants/#{merchant.id}/discounts"
+
+    holidays = HolidayFacade.next_holidays
+    expect(page).to have_content(holidays.first.name)
   end
 end
